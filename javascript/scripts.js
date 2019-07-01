5195a8a1a30c10dafa91f879aab91c3da0963b6f
Will Jones <wsjones1101@gmail.com>
2019-07-01 21:32:28 +0100

    updates on 1st July

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,4 @@
+// Nav hamburgher
 function classToggle() {
   const navs = document.querySelectorAll('.Navbar__Items')
   
@@ -6,3 +7,22 @@ function classToggle() {
 
 document.querySelector('.Navbar__Link-toggle')
   .addEventListener('click', classToggle);
+
+// Tab
+(function(){
+	function onTabClick(event){
+		var actives = document.querySelectorAll('.active');
+
+	    // deactivate existing active tab and panel
+	    for (var i=0; i < actives.length; i++){
+	      	actives[i].className = actives[i].className.replace('active', '');
+	    }
+
+	    // activate new tab and panel
+	    event.target.parentElement.className += ' active';
+	    document.getElementById(event.target.href.split('#')[1]).className += ' active';
+	}
+
+	var el = document.getElementById('nav-tab');
+	el.addEventListener('click', onTabClick, false);
+})();
